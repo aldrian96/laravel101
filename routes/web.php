f7235ecfb319d6063f93d6c28453dbd6796a920f
aldrian96 <mhmdsaldrian96@gmail.com>
2020-09-02 08:57:38 +0700

    second commit

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+Route::get('/belajar', function () {
+    echo '<h1>Hello world</h1>';
+    echo '<h1>sedang belajar laravel</h1>';
+});
+Route::get('/stok_barang/{jenis?}/{merek?}', function ($a = 'smartphone',$b = 'LG') {
+    return "Cek sisa stok untuk $a $b";
+});
+Route::fallback(function(){
+    return 'Maaf halaman yang anda tuju tidak ditemukan';
+});
+
